f8395b2fb21a3562aab961d48d175b7d39d143c8
Eric Stern <firehed@gmail.com>
2009-12-06 05:43:52 +0000

    New ignores

--- private/application/logs/2009-11-11.log.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2009-11-11 19:38:46 -05:00 --- error: Missing i18n entry errors.session.login for language en_US
-2009-11-11 19:38:50 -05:00 --- error: Missing i18n entry titles.blog.index for language en_US
-2009-11-11 19:40:58 -05:00 --- error: Missing i18n entry titles.blog.index for language en_US
