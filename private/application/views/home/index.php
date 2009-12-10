db0529aa7a1591e74405f91adc1a9b65d625bce3
Eric Stern <firehed@gmail.com>
2009-12-10 03:20:38 +0000

    Forum links on home page

--- private/application/views/home/index.php
+++ private/application/views/home/index.php
@@ -3,7 +3,7 @@
 	<p>If every team in FIRST Robotics recruited just two more girls to their team for the 2009/2010 season that would mean 4,000 more girls involved in FIRST!  </p>
 	<h3><a href="signup" title="<?php echo Kohana::lang('site.nav.signup.title'); ?>">Sign up</a> to let everyone know how many girls you recruited to your team and what you did to recruit them!</h3>
 </li>
-<li>If you are a high school girl and you have decided at this time you want to study a subject in the Science, Technology, Engineering, or Math (STEM) area, (forum link)[let us know] what you plan to study and how you came to this decision.</li>
-<li>If you are a high school girl involved in FIRST, but have decided not to go towards a STEM career, please (forum link)[let us know why].  These are extremely important stories for us to know!</li>
-<li>If you are thinking of going towards a STEM career, but are unsure at this time and have questions for women that are in STEM careers, please (forum link)[post your question here].</li>
+<li>If you are a high school girl and you have decided at this time you want to study a subject in the Science, Technology, Engineering, or Math (STEM) area, <a href="forum/view/<?php echo Kohana::config('links.forum.stemMajor'); ?>">let us know</a> what you plan to study and how you came to this decision.</li>
+<li>If you are a high school girl involved in FIRST, but have decided not to go towards a STEM career, please <a href="forum/view/<?php echo Kohana::config('links.forum.nonStemMajor'); ?>">let us know why</a>.  These are extremely important stories for us to know!</li>
+<li>If you are thinking of going towards a STEM career, but are unsure at this time and have questions for women that are in STEM careers, please <a href="forum/view/<?php echo Kohana::config('links.forum.careerQuestions'); ?>">post your question here</a>.</li>
 </ul>
