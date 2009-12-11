dbc487414fa1105048cf155c6b349d295816f995
Eric Stern <firehed@gmail.com>
2009-12-11 18:43:52 +0000

    Updated team SQL creation code

--- private/application/models/team.php
+++ private/application/models/team.php
@@ -2,10 +2,12 @@
 /*
 CREATE TABLE `teams` (
   `number` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(64) NOT NULL DEFAULT '',
   `recruited` tinyint(4) NOT NULL DEFAULT '0',
   `methods` text,
   `girls` tinyint(4) NOT NULL DEFAULT '0',
   `size` tinyint(4) NOT NULL DEFAULT '0',
+  `location` tinytext NOT NULL,
   PRIMARY KEY (`number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 */
