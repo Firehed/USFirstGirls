6842fb37e6c3cfdda8d74ca1511e297d961d021e
Eric Stern <firehed@gmail.com>
2010-01-28 05:09:25 +0000

    Team/edit now uses form helper Profile/edit updated for form lang file Language updates

--- private/application/i18n/en_US/form.php
+++ private/application/i18n/en_US/form.php
@@ -2,10 +2,20 @@
 $lang = array(
 	'label' => array(
 		'nickname' => 'Nickname',
-		'teamNumber' => 'Team Number',
+		'team' => array(
+			'girls' => 'Number of girls on your team (total)',
+			'location' => 'Location',
+			'methods' => 'How did you recruit members',
+			'name'   => 'Team Name',
+			'number' => 'Team Number',
+			'recruited' => 'Girls recruited this year',
+			'size' => 'Number of students on your team',
+			'website' => 'Team Website',
+		),
 		'save' => 'Save',
 	),
 	'placeholder' => array(
 		'blank' => '',
+		'website' => 'http://www.example.com',
 	),
 );
--- private/application/views/team/edit.php
+++ private/application/views/team/edit.php
@@ -1,25 +1,11 @@
-<form action="<?php echo url::current(); ?>" method="post">
-	<fieldset>
-		<?php echo form::csrf(); ?>
+<?=form::open()?>
+<?=form::input('team.name', 'name', $team->name)?>
+<?=form::input('team.location', 'location', $team->location)?>		
+<?=form::input('team.recruited', 'recruited', $team->recruited)?>		
+<?=form::textarea('team.methods', 'methods', $team->methods)?>		
 		
-		<label for="name">Team Name</label>
-		<input type="text" name="name" value="<?php echo $team->name; ?>" id="name" />
+<?=form::input('team.girls', 'girlsOnTeam', $team->girls)?>
+<?=form::input('team.size', 'teamSize', $team->size)?>
 		
-		<label for="location">Location</label>
-		<input type="text" name="location" value="<?php echo $team->location; ?>" id="location" />
-		
-		<label for="recruited">Girls recruited this year</label>
-		<input type="text" name="recruited" value="<?php echo $team->recruited; ?>" id="recruited" />
-		
-		<label for="methods">How did you recruit members</label>
-		<textarea name="methods" id="methods" rows="10" cols="40"><?php echo $team->methods; ?></textarea>
-		
-		<label for="girlsOnTeam">Number of girls on your team (total)</label>
-		<input type="text" name="girlsOnTeam" value="<?php echo $team->girls; ?>" id="girlsOnTeam" />
-		
-		<label for="teamSize">Number of students on your team</label>
-		<input type="text" name="teamSize" value="<?php echo $team->size; ?>" id="teamSize" />
-		
-		<button type="submit">Save</button>
-	</fieldset>
-</form>
+<?=form::submit('save')?>
+<?=form::close()?>
