2ab34d5d02f715c6542b01ace4bb483cdcb724c6
Eric Stern <firehed@gmail.com>
2010-01-28 03:43:52 +0000

    Adding form helper Adding label/placeholder i18n for form helper

---
+++ private/application/i18n/en_US/form.php
@@ -0,0 +1,11 @@
+<?php
+$lang = array(
+	'label' => array(
+		'nickname' => 'Nickname',
+		'teamNumber' => 'Team Number',
+		'save' => 'Save',
+	),
+	'placeholder' => array(
+		'blank' => '',
+	),
+);
