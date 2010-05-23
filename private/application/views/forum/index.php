1f9950095b0c519002810f1042207a3ae7165ce3
Eric Stern <firehed@gmail.com>
2010-05-23 21:06:00 +0000

    ID on forum list html in overview

--- private/application/views/forum/index.php
+++ private/application/views/forum/index.php
@@ -12,7 +12,7 @@
 </header>
 <ul id="forums" class="grid_12">
 <?php foreach ($forums as $forum): ?>
-	<li class="forum">
+	<li class="forum" id="forum<?=$forum->id?>">
 		<header>
 			<h2><a href="forum/view/<?php echo $forum->id; ?>"><?php echo $forum->name; ?></a></h2>
 		</header>
