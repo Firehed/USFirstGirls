826881294c43431cda8a224d418116253f39bc1d
Eric Stern <firehed@gmail.com>
2010-05-23 21:04:04 +0000

    Clean up rendered html

--- private/application/views/admin.php
+++ private/application/views/admin.php
@@ -1,8 +1,8 @@
 <nav class="grid_2">
 	<ul>
-	<?php foreach ($this->pages as $page): ?>
-		<?php if ($this->_allowed($page)):  ?><li><a href="admin/<?php echo $page; ?>"  title="<?php echo Kohana::lang("site.admin.$page.title");  ?>"><?php echo Kohana::lang("site.admin.$page.anchor");  ?></a></li><?php endif; ?>
-	<?php endforeach; ?>
+<?php foreach ($this->pages as $page): if ($this->_allowed($page)):  ?>
+		<li><a href="admin/<?php echo $page; ?>" title="<?php echo Kohana::lang("site.admin.$page.title");  ?>"><?php echo Kohana::lang("site.admin.$page.anchor");  ?></a></li>
+<?php endif; endforeach; ?>
 	</ul>
 </nav>
 <div id="adminContent" class="grid_10">
